b851f1e3637c27175e5fa96065b560d4fae00060
Devin Lambert <dlambert@oattravel.com>
2021-02-16 10:16:45 -0500

    PinToTaskbar module

---
+++ PinToTaskbar.psm1
@@ -0,0 +1,95 @@
+﻿
+class PinToTaskBar_Verb 
+{
+    [string]$KeyPath1  = "HKCU:\SOFTWARE\Classes"
+    [string]$KeyPath2  = "*"
+    [string]$KeyPath3  = "shell"
+    [string]$KeyPath4  = "{:}"
+    
+    [Microsoft.Win32.RegistryKey]$Key2 
+    [Microsoft.Win32.RegistryKey]$Key3
+    [Microsoft.Win32.RegistryKey]$Key4
+
+    PinToTaskBar_Verb()
+    {
+        $this.Key2 = (Get-Item $this.KeyPath1).OpenSubKey($this.KeyPath2, $true)
+    }
+    
+
+    [void] InvokePinVerb([string]$target)
+    {
+        Write-Host "Pinning $target to taskbar"
+        $Shell = New-Object -ComObject "Shell.Application"
+        $Folder = $Shell.Namespace((Get-Item $Target).DirectoryName)
+        $Item = $Folder.ParseName((Get-Item $Target).Name)
+        $Item.InvokeVerb("{:}")
+    }
+
+
+
+    [bool] CreatePinRegistryKeys()
+    {
+        $TASKBARPIN_PATH = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\Windows.taskbarpin";
+        $ValueName = "ExplorerCommandHandler"
+        $ValueData = (Get-ItemProperty $TASKBARPIN_PATH).ExplorerCommandHandler
+        
+        Write-Host "Creating Registry Key: $($this.Key2.Name)\$($this.KeyPath3)"
+        $this.Key3 = $this.Key2.CreateSubKey($this.KeyPath3, $true)
+
+        Write-Host "Creating Registry Key: $($this.Key3.Name)\$($this.KeyPath4)"
+        $this.Key4 = $this.Key3.CreateSubKey($this.KeyPath4, $true)
+
+        Write-Host "Creating Registry Key: $($this.Key4.Name)\$($valueName)"
+        $this.Key4.SetValue($ValueName, $ValueData)
+
+        return $true
+    }
+
+    [bool] DeletePinRegistryKeys()
+    {
+        Write-Host "Deleting Registry Key: $($this.Key4.Name)"
+        $this.Key3.DeleteSubKey($this.KeyPath4)
+        if ($this.Key3.SubKeyCount -eq 0 -and $this.Key3.ValueCount -eq 0) 
+        {
+            Write-Host "Deleting Registry Key: $($this.Key3.Name)"
+            $this.Key2.DeleteSubKey($this.KeyPath3)
+        }
+        return $true
+    }
+
+    [bool] Pin([string]$target)
+    {
+        try
+        {
+            $this.CreatePinRegistryKeys()
+            $this.InvokePinVerb($target)
+        }
+        finally
+        {
+            $this.DeletePinRegistryKeys()
+        }
+        return $true
+    }
+
+}
+
+
+function Set-RunAsAdmin([String]$shortcutPath)
+{
+    if(Test-Path $shortcutPath)
+    {
+        Write-Host "Writing RunAsAdmin to $shortcutPath"
+      # https://social.technet.microsoft.com/Forums/en-US/d97ae6ae-9d34-4e78-b8ca-c7a7f31ac80c/modify-shortcuts-advanced-properties-with-powershell 
+
+      $bytes = [System.IO.File]::ReadAllBytes($shortcutPath)
+      $bytes[0x15] = $bytes[0x15] -bor 0x20 
+      [System.IO.File]::WriteAllBytes($shortcutPath,$bytes)
+    }
+    else
+    {
+        Write-Host "DOES NOT EXIST: $shortcutPath"
+    }
+}
+
+
+
---
+++ Setup_05.ps1
@@ -0,0 +1,32 @@
+﻿using module "C:\Users\dlambert\Desktop\Devin PC Setup\PinToTaskbar.psm1"
+
+
+
+$PathTo_TaskBarShortCuts = "C:\Users\dlambert\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar";
+                            
+[PinToTaskBar_Verb] $pin = [PinToTaskBar_Verb]::new();
+
+$pin.Pin("C:\Windows\explorer.exe") 
+$pin.Pin("$env:windir\system32\SnippingTool.exe") 
+$pin.Pin("C:\Windows\explorer.exe") 
+
+$pin.Pin("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe") 
+Set-RunAsAdmin -shortcutPath "$($PathTo_TaskBarShortCuts)\Google Chrome.lnk"
+
+$pin.Pin("C:\Program Files\Notepad++\notepad++.exe") 
+Set-RunAsAdmin -shortcutPath "$($PathTo_TaskBarShortCuts)\notepad++.lnk"
+
+$pin.Pin("$env:windir\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe") 
+Set-RunAsAdmin -shortcutPath "$($PathTo_TaskBarShortCuts)\Windows PowerShell ISE.lnk"
+
+$pin.Pin("C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe") 
+Set-RunAsAdmin -shortcutPath "$($PathTo_TaskBarShortCuts)\Microsoft Visual Studio 2019.lnk"
+
+$pin.Pin("C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe") 
+Set-RunAsAdmin -shortcutPath "$($PathTo_TaskBarShortCuts)\SSMS 18.lnk"
+
+
+$pin.Pin("$env:windir\system32\inetsrv\InetMgr.exe") 
+Set-RunAsAdmin -shortcutPath "$($PathTo_TaskBarShortCuts)\IIS Manager.lnk"
+
+
