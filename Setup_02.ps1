0d0055dc79eac70bee375b997a167ec60b4f9e4a
Devin Lambert <dlambert@oattravel.com>
2021-02-17 13:19:18 -0500

    adding paint.net to common apps

--- Setup_02.ps1
+++ Setup_02.ps1
@@ -1,4 +1,4 @@
 ﻿using module .\Install-Programs.psm1
 
-$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,autohotkey,virtuawin,ditto,visualstudio2019enterprise"
+$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise"
 Install-Programs -ListOfProgramsToInstall $ListOfProgramsToInstall -Restart $true
