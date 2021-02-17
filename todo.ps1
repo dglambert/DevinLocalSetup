ac1692c302e35b843c54224aed9ab2534d793c49
Devin Lambert <dlambert@oattravel.com>
2021-02-17 07:46:02 -0500

    moving todo's to todo file

---
+++ todo.ps1
@@ -0,0 +1,10 @@
+﻿# need to disable windows v with autohotkey before I can assign to ditto 
+#https://superuser.com/questions/493828/ditto-clipboard-manager-cant-set-shortcut-of-winnum-get-error-registeri
+
+
+# Fix for Powershell shifting focus on F8 from editor window 
+# - https://github.com/arcdev/revert-powershellise
+
+
+# Create QA 1 Chrome Profile
+## sync with devin.qa.1@gmail.com / testing123!
