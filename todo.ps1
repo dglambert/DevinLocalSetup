3fb972ba8cc45896ab766689c6c7f86fe80550c8
Devin Lambert <dlambert@oattravel.com>
2021-04-26 15:15:02 -0400

    adding a todo

--- todo.ps1
+++ todo.ps1
@@ -1,4 +1,6 @@
-﻿# need to disable windows v with autohotkey before I can assign to ditto 
+﻿# Remove pinned programs before pinning new ones. 
+
+# need to disable windows v with autohotkey before I can assign to ditto 
 #https://superuser.com/questions/493828/ditto-clipboard-manager-cant-set-shortcut-of-winnum-get-error-registeri
 
 
