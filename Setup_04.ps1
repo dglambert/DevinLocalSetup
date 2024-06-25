7716dca56ec8fe1dc4ede0b0085baf514b2582fa
Gleason-Lambert <Devin.Gleason-Lambert@kmc.elbitsystems-us.com>
2024-06-25 09:55:58 -0400

    automating install of Razer

--- Setup_04.ps1
+++ Setup_04.ps1
@@ -3,8 +3,6 @@
 $ListOfProgramsToInstall = "sql-server-management-studio"
 Install-Programs -ListOfProgramsToInstall $ListOfProgramsToInstall 
 
-
-Write-output "Manually install razer synapse 3 - https://www.razer.com/synapse-3"
 Write-output "Press Enter key to exit."
 
 Read-Host
