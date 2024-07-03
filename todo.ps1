5f96baad808ee4a9cf51fd6ece832530c79bde2f
Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2024-07-03 12:11:28 -0400

    cleaning up old todo

--- todo.ps1
+++ todo.ps1
@@ -1,8 +1,5 @@
 ﻿# Remove pinned programs before pinning new ones. 
 
-# need to disable windows v with autohotkey before I can assign to ditto 
-#https://superuser.com/questions/493828/ditto-clipboard-manager-cant-set-shortcut-of-winnum-get-error-registeri
-
 
 # Fix for Powershell shifting focus on F8 from editor window 
 # - https://github.com/arcdev/revert-powershellise
