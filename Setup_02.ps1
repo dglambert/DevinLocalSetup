59a0ae2ec525dd15268070b4e40c44acf5e259ce
Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2024-07-15 14:09:43 -0400

    adding python to toolchain

--- Setup_02.ps1
+++ Setup_02.ps1
@@ -3,5 +3,5 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
 
-$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,razer-synapse-3,vscode,microsoft-windows-terminal"
+$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,razer-synapse-3,vscode,microsoft-windows-terminal,python"
 Install-Programs -ListOfProgramsToInstall $ListOfProgramsToInstall -Restart $true
