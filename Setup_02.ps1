6836edf8c6d00e002672331d260503ed8b1ac1c4
Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2024-07-17 09:48:35 -0400

    adding nuget to toolchain

--- Setup_02.ps1
+++ Setup_02.ps1
@@ -3,5 +3,5 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
 
-$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,razer-synapse-3,vscode,microsoft-windows-terminal,python"
+$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,razer-synapse-3,vscode,microsoft-windows-terminal,python,nuget.commandline"
 Install-Programs -ListOfProgramsToInstall $ListOfProgramsToInstall -Restart $true
