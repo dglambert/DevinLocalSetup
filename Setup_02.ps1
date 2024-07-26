fa981ac9546185ecce52e63801487bf78f56ec27
Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2024-07-26 15:07:03 -0400

    adding svn to toolchain

--- Setup_02.ps1
+++ Setup_02.ps1
@@ -3,5 +3,5 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
 
-$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,razer-synapse-3,vscode,microsoft-windows-terminal,python,nuget.commandline"
+$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,razer-synapse-3,vscode,microsoft-windows-terminal,python,nuget.commandline,svn"
 Install-Programs -ListOfProgramsToInstall $ListOfProgramsToInstall -Restart $true
