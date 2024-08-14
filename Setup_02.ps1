bb7f192640194112ee0bb3cfac39ddef4c398360
Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2024-08-14 13:31:20 -0400

    refactor so it wasn't so wide

--- Setup_02.ps1
+++ Setup_02.ps1
@@ -2,6 +2,41 @@
 
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
+$ListOfProgramsToInstall_IDEsAndCompilers = @(
+        "visualstudio2019enterprise"
+        , "visualstudio2022professional"
+        , "vscode"
+        , "python"
+    )
 
-$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,visualstudio2022professional,razer-synapse-3,vscode,microsoft-windows-terminal,python,nuget.commandline,svn,azure-cli,rdcman"
+$ListOfProgramsToInstall_DevelopmentTools = @(
+        "gitextensions"
+        , "dotpeek"
+        , "microsoft-windows-terminal"
+        , "nuget.commandline"
+        , "svn"
+        , "azure-cli"
+    )
+
+$ListOfProgramsToInstall_CommonApplications = @(
+        "zoom"
+        , "googlechrome"
+        , "slack"
+        , "autohotkey"
+        , "virtuawin"
+        , "ditto"
+        , "paint.net"
+        , "razer-synapse-3"
+    )
+
+$ListOfProgramsToInstall_Utilities = @(
+        "notepadplusplus"
+        , "kdiff3"
+        , "7zip"
+        , "rdcman"
+    )
+
+
+$ListOfProgramsToInstall = $($ListOfProgramsToInstall_IDEsAndCompilers ; $ListOfProgramsToInstall_DevelopmentTools ; $ListOfProgramsToInstall_CommonApplications ; $ListOfProgramsToInstall_Utilities) -join ","
+
 Install-Programs -ListOfProgramsToInstall $ListOfProgramsToInstall -Restart $true
