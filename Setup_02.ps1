0d27093e623d9a14b8609f8aa50bd8f619150b74
Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2024-08-14 13:16:25 -0400

    adding vs 2022 pro

--- Setup_02.ps1
+++ Setup_02.ps1
@@ -3,5 +3,5 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
 
-$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,razer-synapse-3,vscode,microsoft-windows-terminal,python,nuget.commandline,svn,azure-cli,rdcman"
+$ListOfProgramsToInstall = "kdiff3,notepadplusplus,7zip,zoom,googlechrome,slack,gitextensions,dotpeek,autohotkey,virtuawin,ditto,paint.net,visualstudio2019enterprise,visualstudio2022professional,razer-synapse-3,vscode,microsoft-windows-terminal,python,nuget.commandline,svn,azure-cli,rdcman"
 Install-Programs -ListOfProgramsToInstall $ListOfProgramsToInstall -Restart $true
