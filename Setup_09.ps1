bd774cd3dbe9bc1d074707ec150e9b2a21241c0f
Devin Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2024-09-17 15:50:16 -0400

    adding powershell profile and copy script (not working at the moment)

---
+++ Setup_09.ps1
@@ -0,0 +1,15 @@
+﻿using module .\Log-SimpleFile.psm1
+
+$LOG_PATH = "C:\localsetuplog.txt"
+
+$CurrentUserPowerShellProfilePath = "$($env:USERPROFILE)\Documents\WindowsPowerShell"
+
+Log-SimpleFile -Path $LOG_PATH -Message "Copying 'Microsoft.PowerShell_profile.ps1' to '$CurrentUserPowerShellProfilePath'"
+
+#Write-Host "Source: $PWD\Microsoft.PowerShell_profile.ps1" 
+#Write-Host "Destination: $CurrentUserPowerShellProfilePath" 
+
+Copy-Item -Path "$PWD\Microsoft.PowerShell_profile.ps1" -Destination "$CurrentUserPowerShellProfilePath" -Verbose
+#robocopy "$PWD" "$CurrentUserPowerShellProfilePath" "Microsoft.PowerShell_profile.ps1"
+
+# This is not working, leaving for now as a reminder to add ps1 to profile directory
