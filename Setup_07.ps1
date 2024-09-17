96b29dffcf1e0a13fff75681685aec3d5d7a410c
Devin Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2024-09-17 14:41:28 -0400

    adding code for disabling right corner right click, two finger click only

--- Setup_07.ps1
+++ Setup_07.ps1
@@ -13,6 +13,12 @@ Set-ItemProperty -Path $KeyPath -Name "SnapFill" -Value 0
 Log-SimpleFile -Path $LogPath -Message "Disabling - System > Multitasking > When I snap a window, simultaneously resize any adjacent snapped window."
 Set-ItemProperty -Path $KeyPath -Name "JointResize" -Value 0
 
+
+$keyPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad"
+Log-SimpleFile -Path $LogPath -Message "Disabling - Settings > Devices > Touchpad > Press the lower right corner of the touchpad to right-click"
+Set-ItemProperty -Path $keyPath -Name "RightClickZoneEnabled" -Value 0
+
+
 Write-Output "Restarting Computer. Hit Enter Key to Continue."
 Read-Host
 
