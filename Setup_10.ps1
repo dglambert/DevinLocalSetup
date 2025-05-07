9c95231412bdef17258a167e14a4be96ab77a698
Devin Gleason-Lambert <dglambert@outlook.com>
2025-05-07 14:08:17 -0400

    adding setup for virtuawin

---
+++ Setup_10.ps1
@@ -0,0 +1,14 @@
+﻿using module .\Log-SimpleFile.psm1
+
+$LOG_PATH = "C:\localsetuplog.txt"
+
+
+Log-SimpleFile -Path $LOG_PATH -Message "Copying repo '\VirtuaWin' to ''"
+
+#Write-Host "Source: $PWD\Microsoft.PowerShell_profile.ps1" 
+#Write-Host "Destination: $CurrentUserPowerShellProfilePath" 
+
+Copy-Item -Path "$PWD\VirtuaWin" -Destination "$env:APPDATA" -Recurse -Force -Verbose
+#robocopy "$PWD" "$CurrentUserPowerShellProfilePath" "Microsoft.PowerShell_profile.ps1"
+
+# This is not working, leaving for now as a reminder to add ps1 to profile directory
