70085406cab346c8f82fdbb10aac053a7e258dcc
Devin Gleason-Lambert <Devin.GleasonLambert@kmcsystems.com>
2025-05-07 19:27:36 -0400

    initial commit for add to startup

--- Setup_10.ps1
+++ Setup_10.ps1
@@ -3,12 +3,20 @@
 $LOG_PATH = "C:\localsetuplog.txt"
 
 
-Log-SimpleFile -Path $LOG_PATH -Message "Copying repo '\VirtuaWin' to ''"
+Copy-Item -Path "$PWD\VirtuaWin" -Destination "$env:APPDATA" -Recurse -Force -Verbose
+Log-SimpleFile -Path $LOG_PATH -Message "Copied repo '\VirtuaWin' to '$env:APPDATA'"
+
+Copy-Item -Path "$PWD\Map-WinV-to-Ditto.ahk" -Destination "$($HOME)\Desktop" -Force -Verbose
+Log-SimpleFile -Path $LOG_PATH -Message "Copied repo '\Map-WinV-to-Ditto.ahk' to '$($HOME)\Desktop'"
+
 
 #Write-Host "Source: $PWD\Microsoft.PowerShell_profile.ps1" 
 #Write-Host "Destination: $CurrentUserPowerShellProfilePath" 
 
-Copy-Item -Path "$PWD\VirtuaWin" -Destination "$env:APPDATA" -Recurse -Force -Verbose
+
+
 #robocopy "$PWD" "$CurrentUserPowerShellProfilePath" "Microsoft.PowerShell_profile.ps1"
 
 # This is not working, leaving for now as a reminder to add ps1 to profile directory
+
+$HOME
---
+++ Setup_11.ps1
@@ -0,0 +1,26 @@
+using module .\Log-SimpleFile.psm1
+using module .\AddToStartup.psm1
+
+$LOG_PATH = "C:\localsetuplog.txt"
+
+
+$paths_to_add_to_startup = @(
+    @{
+        Path = "C:\Program Files (x86)\Razer\Synapse3\WPFUI\Framework\Razer Synapse 3 Host\Razer Synapse 3.exe"
+    }
+    , @{
+        Path = "C:\Program Files\AutoHotkey\AutoHotkeyU64.exe"
+        Argument = "$($HOME)\Desktop\Map-WinV-to-Ditto.ahk"
+    }
+)
+
+foreach($startupApp in $paths_to_add_to_startup)
+{
+    Add-CurrentUserStartupAppToRegistry -ApplicationPath $startupApp.Path -Argument $startupApp.Argument
+    
+    Log-SimpleFile -Path $LOG_PATH -Message "Added '$($startupApp.Path)' to startup"
+
+}
+
+
+
