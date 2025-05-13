ad93a9549ca70aaf7a98f4b37c164e0fe2b467ae
Devin Gleason-Lambert <dglambert@outlook.com>
2025-05-13 11:52:39 -0400

    Reverted back to starting up via task scheduler

--- Setup_11.ps1
+++ Setup_11.ps1
@@ -4,37 +4,14 @@ using module .\AddToStartup.psm1
 $LOG_PATH = "C:\localsetuplog.txt"
 
 
-$paths_to_add_to_startup = @(
-    
-    @{
-        Path = "C:\Program Files\AutoHotkey\AutoHotkeyU64.exe"
-        Argument = "$($HOME)\Desktop\Map-WinV-to-Ditto.ahk"
-    }
-    
-    
-    # , @{
-    #     Path = "C:\Program Files (x86)\Razer\Synapse3\WPFUI\Framework\Razer Synapse 3 Host\Razer Synapse 3.exe"
-    #     Argument = "/StartMinimized"
-    # }
-
-    # , @{
-    #     Path = "C:\Program Files\Ditto\Ditto.exe"
-    # }
-
-    # , @{
-    #     Path = "C:\Program Files (x86)\VirtuaWin\VirtuaWin.exe"
-    # }
-
-)
-
-foreach($startupApp in $paths_to_add_to_startup)
+foreach($scheduled_task_xml in "$PWD\Scheduled Tasks" | Get-ChildItem )
 {
-    Add-CurrentUserStartupAppToRegistry -ApplicationPath $startupApp.Path -Argument $startupApp.Argument
-    
-    Log-SimpleFile -Path $LOG_PATH -Message "Added '$($startupApp.Path)' to startup"
+    Register-ScheduledTask -TaskPath "\" `
+                            -TaskName $scheduled_task_xml.BaseName `
+                            -XML (Get-Content $scheduled_task_xml.FullName -raw)
 
+    Log-SimpleFile -Path $LOG_PATH -Message "Importing Scheduled Tasks: $($scheduled_task_xml.FullName)"
 }
-
 
 
 Write-output "Press Enter key to exit."
